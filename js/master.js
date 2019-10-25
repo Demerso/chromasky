6dfc627883b8ce8acb1d90c7d5925410c0b96611
Olivier Demers <olidem123@gmail.com>
2019-10-25 15:01:17 -0400

    Created Menu

--- js/master.js
+++ js/master.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-  $("#back, body").vegas({
+  makeSquares("#menu");
+
+  $("#menu")
+    .children()
+    .mouseenter(function(event) {
+      changeBack("enter", this);
+    })
+    .mouseleave(function(event) {
+      changeBack("leave", this);
+    }); //
+
+  $("body").vegas({
     slides: [
-      { src: "img/slideshow/img1.png" },
-      { src: "img/slideshow/img2.png" },
-      { src: "img/slideshow/img3.png" },
-      { src: "img/slideshow/img4.png" },
-      { src: "img/slideshow/img5.png" },
-      { src: "img/slideshow/img6.png" }
+      { src: "./img/slideshow/img1.png" },
+      { src: "./img/slideshow/img2.png" },
+      { src: "./img/slideshow/img3.png" },
+      { src: "./img/slideshow/img4.png" },
+      { src: "./img/slideshow/img5.png" },
+      { src: "./img/slideshow/img6.png" }
     ],
     color: "black",
     transition: "fade",
@@ -17,3 +28,28 @@ $(document).ready(function() {
     timer: false
   });
 });
+
+$(window).resize(function() {
+  makeSquares("#menu");
+});
+
+function makeSquares(id) {
+  var width = $(id)
+    .children()
+    .css("width");
+  $(id)
+    .children()
+    .css("height", width);
+}
+
+function changeBack(state, element) {
+  var num = Math.floor(Math.random() * 4) + 1;
+  if (state == "enter") {
+    $(element).css(
+      "background-image",
+      "url(img/buttons/btn".concat(num, ".png)")
+    );
+  } else {
+    $(element).css("background-image", "url(img/stone.png)");
+  }
+}

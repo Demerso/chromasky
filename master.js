af9ce2dfebc57995b32f974b2361a3e8680ff40d
Olivier Demers <olidem123@gmail.com>
2019-10-25 09:35:44 -0400

    Prettify

--- master.js
+++ master.js
@@ -1,19 +1,19 @@
-$(document).ready(function()
-  {
-    $("#back, body").vegas({
-        slides: [
-            { src: "img/slideshow/img1.png" },
-            { src: "img/slideshow/img2.png" },
-            { src: "img/slideshow/img3.png" },
-            { src: "img/slideshow/img4.png" },
-            { src: "img/slideshow/img5.png" },
-            { src: "img/slideshow/img6.png" }],
-        color: 'black',
-        transition: 'fade',
-        preloadImage: true,
-        shuffle: true,
-        delay: 5000,
-        animation: 'kenburns',
-        timer: false
-    });
-  })
+$(document).ready(function() {
+  $("#back, body").vegas({
+    slides: [
+      { src: "img/slideshow/img1.png" },
+      { src: "img/slideshow/img2.png" },
+      { src: "img/slideshow/img3.png" },
+      { src: "img/slideshow/img4.png" },
+      { src: "img/slideshow/img5.png" },
+      { src: "img/slideshow/img6.png" }
+    ],
+    color: "black",
+    transition: "fade",
+    preloadImage: true,
+    shuffle: true,
+    delay: 5000,
+    animation: "kenburns",
+    timer: false
+  });
+});
